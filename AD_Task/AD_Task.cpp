376cbddf97bb0958b4574bfad778689d9289bbd1
BlackOrangee <doktorzoidberg8@gmail.com>
2023-05-27 12:34:05 +0300

    Write a program that removes empty rows from a two-dimensional array

--- AD_Task/AD_Task.cpp
+++ AD_Task/AD_Task.cpp
@@ -4,88 +4,73 @@
 #include <iostream>
 using namespace std;
 
-void GenerateArray(int** arr, int& size);
-void PrintArray(int** arr, int& size);
-int** EmptyRowsRemover(int** arr, int* size)
-{
-	int size2 = *size;
-	int** arr_new = new int* [size2];
-	int k = 0;
-
-	for (int i = 0; i < size2 - 1; i++)
-	{
-		for (int j = 0; j < arr[i][0]; j++)
-		{
-			if (arr[i][0] == 0)
-			{
-				k++;
-				size2--;
-			}
-			else
-			{
-				arr_new[i] = new int[arr[i + k][0]];
-				arr_new[i][0] = arr[i + k][0];
-				arr_new[i][j] == arr[i + k][j];
-			}
-
-		}
-	}
-	delete arr;
-	*size = size2;
-	return arr_new;
-}
-
+char** GenerateArray(int& size);
+void PrintArray(char** arr, int& size);
+char** EmptyRowsRemover(char** str, int& size);
 
 int main()
 {
-	srand(time(NULL));
-
 	printf("\n\n\tEnter strings number ");
 	int size;
 	cin >> size;
 
-	int** arr = new int* [size];
+	char** str = GenerateArray(size);
 
-	GenerateArray(arr, size);
+	printf("\n\n\tEntered string: \n");
+	PrintArray(str, size);
 
-	printf("\n\n\tArray: \n");
-	PrintArray(arr, size);
+	char** str_new = EmptyRowsRemover(str, size);
 
-	int** arr_new = EmptyRowsRemover(arr, &size);
-
-	printf("\n\n\tArray: \n");
-	PrintArray(arr_new, size);
+	printf("\n\n\tCorrected string: \n");
+	PrintArray(str_new, size);
 }
 
-void GenerateArray(int** arr, int& size)//Generate array
+char** GenerateArray(int& size)//Generate array
 {
-
+	char** str = new char* [size];
 	for (int i = 0; i < size; i++)
 	{
-		printf("\tEnter string %d size ", i + 1);
-		int str_size = 0;
-		cin >> str_size;
-
-		str_size++;
-		arr[i] = new int[str_size];
-		arr[i][0] = str_size;
-
-		for (int j = 1; j < str_size; j++)
-		{
-			arr[i][j] = rand() % 10;
-		}
+		int str_size = 10;
+		str[i] = new char[str_size];
+		
+		cout << "\n\tEnter string, use '0' to skip\n\t";	cin >> str[i];
 	}
+	return str;
 }
 
-void PrintArray(int** arr, int& size)//Print array
+void PrintArray(char** arr, int& size)//Print array
 {
 	for (int i = 0; i < size; i++)
 	{
-		cout << "\t";
-		for (int j = 1; j < arr[i][0]; j++)
-		{
-			cout << arr[i][j] << " ";
-		}
-		cout << "\n";
+		cout << "\t" << arr[i] << "\n";
 	}
+}
+
+char** EmptyRowsRemover(char** str, int& size)
+{
+	int size_new = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (str[i][0] != '0')
+		{
+			size_new++;
+		}
+	}
+	char** str_new = new char* [size_new];
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (str[i][0] == '0')
+		{
+			swap(str[i], str[i + 1]);
+		}
+	}
+	for (int i = 0; i < size_new; i++)
+	{
+		str_new[i] = str[i];
+	}
+	size = size_new;
+	delete[] str;
+	return str_new;
 }
